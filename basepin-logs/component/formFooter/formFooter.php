4187e83fef96f191c47c6e4068d98b966a9633c5
Zxhc <zxhc123@gmail.com>
2026-04-07 23:12:40 +0800

    Delete basepin-logs directory

--- basepin-logs/component/formFooter/formFooter.php
+++
@@ -1,4 +0,0 @@
-<div class="button-container">
-<button type="submit" id="submitBtn"><span class="material-icons-outlined">check_circle</span>Submit Form</button>
-<button id="rstBtn"><span class="material-icons-outlined">cancel</span>Reset Form</button>
-</div>
