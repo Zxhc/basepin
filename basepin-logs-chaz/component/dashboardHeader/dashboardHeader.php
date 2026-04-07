8532872e5ead0eabb1e5ecfec9f7eea9b0231a59
Zxhc <zxhc123@gmail.com>
2026-04-07 15:43:03 +0800

    Delete basepin-logs-chaz directory

--- basepin-logs-chaz/component/dashboardHeader/dashboardHeader.php
+++
@@ -1,8 +0,0 @@
- <div class="form-header">
-    <div class="header-text"><h1>INPUT FORM</h1></div>
-        <div class="toggle-container">
-            <div class="slider" id="slider"></div>
-            <div class="tab active" onclick="moveToggle(0, 'form', this)">Input Form</div>
-            <div class="tab" onclick="moveToggle(1, 'logs', this)">View Logs</div>
-        </div>  
-  </div>
